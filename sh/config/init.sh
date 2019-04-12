8393b977723fdd1e215c9e5d8b8096a54423309e
maany <imptodefeat@gmail.com>
2019-04-12 12:14:03 +0200

    add init.sh and run scripts

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -15,36 +15,6 @@ cat /etc/simple_grid/config/hadoop_env.sh >> ~/.bashrc
 cat /etc/simple_grid/config/spark_hadoop_env.sh >> ~/.bashrc
 source ~/.bashrc
 
-echo "Formatting HDFS"
-hdfs namenode -format
 
-echo "Adding known_hosts"
-
-file="/etc/simple_grid/config/slaves_ip"
-for var in $(cat $file)
-do
-ssh-keyscan -H $var >> ~/.ssh/known_hosts
-echo " $var"
-done
-
-echo "Starting HDFS"
-start-dfs.sh
-
-echo "Start YARN"
-start-yarn.sh
-
-echo "HDFS report"
-hdfs dfsadmin -report
-
-echo "YARN nodes"
-yarn node -list
-
-echo "Initializing the Spark Hadoop Master Container"
-
-echo "Creating spark-logs hdfs directory"
-hdfs dfs -mkdir /spark-logs
-
-echo "Start Spark History Server"
-$SPARK_HOME/sbin/start-history-server.sh
 
 echo "All Done!"

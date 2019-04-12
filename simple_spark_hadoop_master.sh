d85328c4a44b712b4f8f420570082183cac8bd62
maany <imptodefeat@gmail.com>
2019-04-12 11:50:39 +0200

    update run script

--- simple_spark_hadoop_master.sh
+++ simple_spark_hadoop_master.sh
@@ -17,6 +17,10 @@ sudo docker run -itd \
     -p 50070:50070 \
     -p 18080:18080 \
     -p 9000:9000 \
+    --net spark_tests \
+    --ip 10.1.1.10 \
+    --add-host "spark_hadoop_worker_localhost01_1.cern.ch:10.1.1.11" \
+    --hostname simple-lc01.cern.ch \
     -v $(pwd)/sh/config:/etc/simple_grid/config \
     -v $(pwd)/augmented_site_level_config_file.yaml:/etc/simple_grid/augmented_site_level_config_file.yaml \
     simple_spark_hadoop_master

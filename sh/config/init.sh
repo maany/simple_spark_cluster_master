71caa064b6dc1a3f30233ded490efc3129caa3ff
maany <imptodefeat@gmail.com>
2019-04-12 14:16:51 +0200

    permission for host key

--- sh/config/init.sh
+++ sh/config/init.sh
@@ -18,6 +18,7 @@ cp -f /etc/simple_grid/config/id_rsa.pub /root/.ssh/authorized_keys
 cp -f /etc/simple_grid/config/ssh_host_rsa_key /etc/ssh/
 cp -f /etc/simple_grid/config/ssh_host_rsa_key.pub /etc/ssh/
 chmod 600 ~/.ssh/id_rsa
+chmod 640 /etc/ssh/ssh_host_rsa_key
 cat /etc/simple_grid/config/hadoop_env.sh >> ~/.bashrc
 cat /etc/simple_grid/config/spark_hadoop_env.sh >> ~/.bashrc
 source ~/.bashrc

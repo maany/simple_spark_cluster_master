43e09c401b137c73b8c74ca9f2c575cbf83a2dfe
maany <imptodefeat@gmail.com>
2019-04-15 15:43:52 +0200

    update hostname in run script

--- run_simple_spark_hadoop_master.sh
+++ run_simple_spark_hadoop_master.sh
@@ -20,7 +20,7 @@ sudo docker run -itd \
     --net spark_tests \
     --ip 10.1.1.10 \
     --add-host "spark_hadoop_worker_localhost01_1.cern.ch:10.1.1.11" \
-    --hostname simple-lc01.cern.ch \
+    --hostname $(hostname) \
     -v $(pwd)/sh/config:/etc/simple_grid/config \
     -v $(pwd)/augmented_site_level_config_file.yaml:/etc/simple_grid/augmented_site_level_config_file.yaml \
     simple_spark_hadoop_master
--- sh/config/late_init.sh
+++ sh/config/late_init.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 echo "Copy ssh host keys for workers. Should be in a for loop:"
-ssh-keyscan -t rsa spark_hadoop_worker_localhost01_1.cern.ch >> /etc/ssh/ssh_known_hosts
+file="/etc/simple_grid/config/slaves_ip"
+for var in $(cat $file)
+do
+ssh-keyscan -t rsa $var >> /etc/ssh/ssh_known_hosts
+echo " $var"
+done
 
 ssh-keyscan simple-lc01.cern.ch >> /etc/ssh/ssh_known_hosts
 ssh-keyscan 0.0.0.0 >> /etc/ssh/ssh_known_hosts
 
 echo "Formatting HDFS"
 hdfs namenode -format
-
-echo "Adding known_hosts"
-
-file="/etc/simple_grid/config/slaves_ip"
-for var in $(cat $file)
-do
-ssh-keyscan -H $var >> ~/.ssh/known_hosts
-echo " $var"
-done
 
 echo "Starting HDFS"
 start-dfs.sh
